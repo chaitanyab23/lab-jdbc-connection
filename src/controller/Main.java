3bed8a6028f1df9cbd316fa4360febc85be12637
chaitanyab23 <chaitanyabhagat23@gmail.com>
2020-09-08 15:01:04 +0530

    chaitanya

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,9 +5,20 @@ import java.sql.Connection;
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		
 	// Fill your code
+		Connection con= null;
 		
+		con=ConnectionManager.getConnection();
+		
+		if (con!= null)
+		{
+			System.out.println("Connection Established");
+		}
+		else
+		{
+			System.out.println("Check Connection");
+		}
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,26 @@
 package utility;
 
-
-
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 public class ConnectionManager {
-	
-	
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close();
+		return prop;
+	}
+
+	public static Connection getConnection() throws Exception {
+		// TODO Auto-generated method stub
+		Properties p=loadPropertiesFile();
+		Connection con=null;
+		con = DriverManager.getConnection(p.getProperty("url"), p.getProperty("username"), p.getProperty("password"));
+		
+		return con;
+	}
+
 }
